cd69b085acb3aabfa3cb6da75a1c66682a76a528
Emre Üçbudak <147994857+emreucbudak@users.noreply.github.com>
2024-08-26 20:13:35 +0300

    Add files via upload

---
+++ main (4).php
@@ -0,0 +1,12 @@
+<?php
+/******************************************************************************
+
+    Emre Üçbudak Karadeniz Teknik Üniversitesi
+
+*******************************************************************************/
+//  dizi birleştirmek için array_merge
+$dizi = array("emre","üçbudak");
+$dizi1 = array("ktü");
+$yenidizi = array_merge($dizi,$dizi1);
+echo($yenidizi[0])
+?>
